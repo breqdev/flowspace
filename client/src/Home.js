126f08fdb1131b55622bdbb0b13c4fb7d9b4bb63
Brooke Chalmers <breq@breq.dev>
2021-06-29 03:31:03 -0400

    improve home page a bit

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,7 +1,15 @@
 export default function Home(props) {
     return (
         <div className="container mx-auto px-4">
-            <h1>Hello World!</h1>
+            <h1 className="text-6xl md:text-9xl text-center my-8">flowspace</h1>
+            <h2 className="text-4xl text-center my-4">welcome to a <span className="text-blue-500">new space</span> to talk.</h2>
+
+            <p className="w-96 mx-auto text-xl text-center my-8">
+                flowspace connects you to your friends like never before.
+                it's a space where ideas, thoughts, and experiences can flow
+                freely. it's a haven from the world of gatekeeping algorithms
+                and intrusive advertising. just you and your community. flowspace.
+            </p>
         </div>
     )
 }
