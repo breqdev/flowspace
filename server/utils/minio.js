351851ad8802b8d478623d799d7dd7b653b10cff
Brooke Chalmers <breq@breq.dev>
2021-07-20 02:58:17 -0400

    Parse port env var as int

--- server/utils/minio.js
+++ server/utils/minio.js
@@ -2,7 +2,7 @@ const Minio = require("minio")
 
 const minio = new Minio.Client({
     endPoint: process.env.MINIO_ENDPOINT,
-    port: process.env.MINIO_PORT || 9000,
+    port: parseInt(process.env.MINIO_PORT) || 9000,
     useSSL: (process.env.MINIO_USE_SSL === "true"),
     accessKey: process.env.MINIO_ACCESS_KEY,
     secretKey: process.env.MINIO_SECRET_KEY
