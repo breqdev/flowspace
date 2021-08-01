e98b8ed32f7a84563ea347eb4dc764b802241ff5
Brooke Chalmers <breq@breq.dev>
2021-08-01 18:41:05 -0400

    Remove profile modules temporarily

--- client/src/LoggedInView/Profile/Profile.js
+++ client/src/LoggedInView/Profile/Profile.js
@@ -3,7 +3,6 @@ import { Link, Redirect } from "react-router-dom"
 
 
 import { useAPI } from "../../api.js"
-import { ImageModule, MarkdownModule, ModuleGrid, VideoModule } from "./Module.js"
 
 
 import UserCard from "./UserCard.js"
@@ -28,38 +27,9 @@ export default function Profile(props) {
         )
     }
 
-    const markdownContent = `This is example markdown content
-
-And so is this
-... on the same paragraph
-
-
-And a different paragraph
-
-
-* This is a list
-* This is another list item
-
-Links are cool [hi](https://google.com)
-
-
-tables?
-
-| col | col | col |
-|-----|-----|-----|
-| row | row | row |
-
-`
-
     return (
         <div className="max-w-6xl mx-auto w-full">
             <UserCard user={data} />
-
-            <ModuleGrid>
-                <MarkdownModule content={markdownContent} />
-                <ImageModule src="https://cdn.pixabay.com/photo/2013/07/12/17/47/test-pattern-152459_960_720.png" caption="Test Image" url="https://google.com" />
-                <VideoModule />
-            </ModuleGrid>
         </div>
     )
 }
