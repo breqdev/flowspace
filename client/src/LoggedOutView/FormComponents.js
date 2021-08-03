4c99fbd4b5faf2d7d1291ff8f01fa55fb01e3d03
Brooke Chalmers <breq@breq.dev>
2021-08-02 21:03:41 -0400

    Show errors in the input component

--- client/src/LoggedOutView/FormComponents.js
+++ client/src/LoggedOutView/FormComponents.js
@@ -1,9 +1,23 @@
 export function Input(props) {
+    const error = props.form.touched[props.field.name] && props.form.errors[props.field.name]
+
     return (
-        <input size="1"
-        className="rounded-full border-2 border-gray-200 focus:border-blue-500 outline-none px-6 py-4 my-2"
-        {...props} {...props.field}
-        />
+        <div className="relative">
+            <input size="1"
+            className={
+                "w-full px-6 py-4 my-2 rounded-full border-2 " +
+                "focus:border-blue-500 outline-none " +
+                (error ? "border-red-400" : "border-gray-200")
+            }
+            {...props} {...props.field}
+            />
+            <div className="absolute bottom-0 left-0 right-0 -mb-1 text-lg text-red-600"
+            style={{ display: error ? 'block' : 'none'}}>
+                <span className="rounded-full border-2 border-red-400 px-4 bg-white">
+                    {error}
+                </span>
+            </div>
+        </div>
     )
 }
 
